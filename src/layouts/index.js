f206f5a04cf852b7f79ba1cb5dd431cf08cb7f6c
zackcreach <zackcreach@gmail.com>
2017-09-11 20:17:15 -0400

    Added placeholder info to index.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -36,21 +36,16 @@ const Header = () => (
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet
+    {/* <Helmet
       title="Gatsby Default Starter"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
+    <Header /> */}
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
+      className="index__center"
     >
       {children()}
     </div>
